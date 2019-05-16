01339199975ca81558e876a22446d63144c5e598
Khutso95 <47758109+Khutso95@users.noreply.github.com>
2019-05-16 12:15:54 +0200

    Update test_find_exams.py

--- test_find_exams.py
+++ test_find_exams.py
@@ -14,6 +14,11 @@ class FindExamsTest(unittest.TestCase):
 	def test_getExams(self):
 		MyDict = {'ELEN4013': ('2020-06-01', 'SHWWB')}
 		self.assertDictEqual(FE.getExams('examlist.csv'),MyDict)
+	def test_getTimeTable(self):
+		timeT = [('ELEN4013', [('2020-06-01', ('TBD', 'TBD')), ('SHWWB', ('TBD', 'TBD'))])]
+		list2 = FE.getTimeTable({'ELEN4013': ('2020-06-01', 'SHWWB')},'examlist.csv')
+		self.assertListEqual(list2,timeT)
+
 
 if __name__ == "__main__":
 	unittest.main()
