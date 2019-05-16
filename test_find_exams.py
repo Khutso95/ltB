120069bdff180ba77a208f22ace9d97de0e8eb84
Khutso Nkadimeng <884200@students.wits.ac.za>
2019-05-16 12:23:46 +0200

    last

--- test_find_exams.py
+++ test_find_exams.py
@@ -6,10 +6,7 @@ class FindExamsTest(unittest.TestCase):
 	def test_getCourse(self):
 		
 		self.assertTupleEqual(FE.getCourse("Levitt/ELEN4010"),('Levitt', 'ELEN4010'))
-	def test_getCoursesForLects(self):
-		#Dict = {'Cronje': ['ELEN4014'], 'Hazelhurst': ['ELEN7039', {...}, {...}], 'Dinger': 		#['SCMD1003'], 'Hofsajer': ['ELEN3003', {...}], 'Levitt': ['ELEN4010', {...}]}
-		Dict = {'Cronje': ['ELEN4014']}
-		self.assertDictEqual(FE.getCoursesForLects("lectlist.csv"), Dict)
+	
 	
 	def test_getExams(self):
 		"""comparing two dictionaries"""
