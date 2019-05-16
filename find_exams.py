03b4dba0a1db36c0d4cec4deddfcf7d187f66289
Khutso Nkadimeng <884200@students.wits.ac.za>
2019-05-16 10:48:18 +0200

    Commit 4.3

--- find_exams.py
+++ find_exams.py
@@ -5,10 +5,12 @@ import re
 
 
 def getCourse(line):
+    """returns course name and the lecturer"""
     l_name, c_name = line.rstrip().split(",")    ##EE1
     return c_name, l_name   #EE2
 
 def getCoursesForLects(lectsfn):
+    """for each lecturer the function returns a dictionary list of courses"""	
     courses = {}  # dictionary: for each lect returns list of courses
     lf = open(lectsfn)
     for line in lf:
@@ -21,6 +23,7 @@ def getCoursesForLects(lectsfn):
     return courses
 
 def getExams(examfname):
+    """input an exam name and the fuction returns date of the exam and the vanue """
     exams = {}
     for line in open(examfname):
         data = line.rstrip().split(",")
@@ -28,6 +31,7 @@ def getExams(examfname):
     return exams
 
 def getTimeTable(courses,exams):
+    """this fuction returns a list of exams for each lecturer"""
     ttable = []  # nested list -- for each lect a list of exams
     for lect in sorted(courses.keys()):
         l_exams   = [] # build list of lecturer's exams
@@ -42,6 +46,7 @@ def getTimeTable(courses,exams):
     return ttable
 
 def showTimeTable(ttable):
+    """prints the timetable"""
     for (lect, l_exams) in ttable:
         print(lect)
         for c,ex in l_exams:
