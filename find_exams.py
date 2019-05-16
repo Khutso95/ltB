10180cca3b18b693ad443017122be88052c86b12
Khutso Nkadimeng <884200@students.wits.ac.za>
2019-05-16 11:55:34 +0200

    test file 1

--- find_exams.py
+++ find_exams.py
@@ -6,8 +6,8 @@ import re
 
 def getCourse(line):
     """returns course name and the lecturer"""
-    l_name, c_name = line.rstrip().split(",")    ##EE1
-    return c_name, l_name   #EE2
+    l_name, c_name = line.rstrip().split("/")    ##EE1
+    return l_name, c_name    #EE2
 
 def getCoursesForLects(lectsfn):
     """for each lecturer the function returns a dictionary list of courses"""	
@@ -16,41 +16,41 @@ def getCoursesForLects(lectsfn):
     for line in lf:
         lect, course = getCourse(line)
         if lect in courses: # is the lecturer in the dictionary
-            courses[lect].append(lect)  # add course to the list ##EE3
+            courses[lect].append(courses)  # add course to the list ##EE3
         else:
             courses[lect] = [course]  
     lf.close()
     return courses
 
 def getExams(examfname):
-    """input an exam name and the fuction returns date of the exam and the vanue """
-    exams = {}
-    for line in open(examfname):
-        data = line.rstrip().split(",")
-        exams[data[0]]=(data[1],data[2])
-    return exams
+	"""input an exam name and the fuction returns date of the exam and the vanue """
+	exams = {}
+	for line in open(examfname):
+		data = line.rstrip().split(",")
+		exams[data[0]]=(data[1],data[2])
+		return exams
 
 def getTimeTable(courses,exams):
-    """this fuction returns a list of exams for each lecturer"""
-    ttable = []  # nested list -- for each lect a list of exams
-    for lect in sorted(courses.keys()):
-        l_exams   = [] # build list of lecturer's exams
-        l_courses = courses[lect] # get their courses
-        for c in l_courses:
-            if c not in exams:
-                the_exam=("TBD","TBD")
-            else:
-                the_exam = exams[c]
-            l_exams.append((c,the_exam))
-        ttable.append((lect,l_exams)) # now we know the exams add it list
-    return ttable
+	"""this fuction returns a list of exams for each lecturer"""
+	ttable = []  # nested list -- for each lect a list of exams
+	for lect in sorted(courses.keys()):
+		l_exams   = [] # build list of lecturer's exams
+		l_courses = courses[lect] # get their courses
+		for c in l_courses:
+			if c not in exams:
+				the_exam=("TBD","TBD")
+			else:
+				the_exam = exams[c]
+			l_exams.append((c,the_exam))
+		ttable.append((lect,l_exams)) # now we know the exams add it list
+	return ttable
 
-def showTimeTable(ttable):
-    """prints the timetable"""
-    for (lect, l_exams) in ttable:
-        print(lect)
-        for c,ex in l_exams:
-            print("   ",c,ex[0],ex[1])
+def showTimeTable(ttabsle):
+	"""prints the timetable"""
+	for (lect, l_exams) in ttable:
+		print(lect)
+		for c,ex in l_exams:
+			print("   ",c,ex[0],ex[1])
         
 
 if __name__ == "__main__":
--- test_find_exams.py
+++ test_find_exams.py
@@ -1,6 +1,19 @@
 import unittest
 import find_exams as FE
 
+#print(FE.getExams('examlist.csv'))
 class FindExamsTest(unittest.TestCase):
-    
-    def first_test(self):
+	def test_getCourse(self):
+		
+		self.assertTupleEqual(FE.getCourse("Levitt/ELEN4010"),('Levitt', 'ELEN4010'))
+	def test_getCoursesForLects(self):
+		#Dict = {'Cronje': ['ELEN4014'], 'Hazelhurst': ['ELEN7039', {...}, {...}], 'Dinger': 		#['SCMD1003'], 'Hofsajer': ['ELEN3003', {...}], 'Levitt': ['ELEN4010', {...}]}
+		Dict = {'Cronje': ['ELEN4014']}
+		self.assertDictEqual(FE.getCoursesForLects("lectlist.csv"), Dict)
+	
+	def test_getExams(self):
+		MyDict = {'ELEN4013': ('2020-06-01', 'SHWWB')}
+		self.assertDictEqual(FE.getExams('examlist.csv'),MyDict)
+
+if __name__ == "__main__":
+	unittest.main()
